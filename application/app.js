d0d8b5f7dfb86e6582ab86c96a3be68c5cf984ef
Tristan <thrawst88@gmail.com>
2015-03-31 15:49:52 -0600

    nice title fetching, form functions

--- application/app.js
+++ application/app.js
@@ -12,21 +12,16 @@ app.get('/', function(req, res){
 });
 
 app.post('/title', function(req, res){
-  debugger;
-  var url = req.query.url;
-  console.log(url);
+  var url = req.body.url;
   request(url, function (err, resquest, body) {
     if (!err && res.statusCode == 200) {
-      body = body.replace(/\n/g,'');
       var title = 'no title found';
+      body = body.replace(/\n/g,'');
       var match=body.match(/<title>(.*)<\/title>/im);
       if (match && match.length>1){
-        title=(''+match[1]).trim();
+        title= (''+match[1]).trim();
       }
-      res.send(title); 
-    }
-    else{
-      res.send(err.toString());
+      res.send({title: title}); 
     }
   });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -11,47 +11,52 @@ var LinksBox = React.createClass({
 });
 
 var AddLinkForm = React.createClass({
+  getInitialState: function() {
+    return {title: '', link:'', description: ''};
+  },
   handleSubmit: function(e){
     e.preventDefault();
-    var title = e.target.title.value.trim();
-    var link = e.target.link.value.trim();
-    var description = e.target.description.value.trim();
 
-    if (!title || !link || !description) {
+    if (!this.state.title || !this.state.link || !this.state.description) {
       return;
     }
 
-    this.props.onLinkSubmit(title, link, description);
+    this.props.onLinkSubmit(this.state);
+    this.setState(this.getInitialState());
   },
-
+  updateTitle: function(e){
+    this.setState({title: e.target.value});
+  },
+  updateDescription: function(e){
+    this.setState({description: e.target.value});
+  },  
   fetchTitle: function(e){
     var link = e.target.value;
     if (link.length == 0){
       return;
     }
-
+    this.setState({title: link});
     $.ajax({
       url: '/title',
-      dataType: 'text',
+      dataType: 'json',
       type: 'POST',
       data: {url: link}, //problem here?
       success: function(data) {
-        this.setState({title: data, link:link});
+        this.setState({title: data.title, link:link});
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error(err.toString());
       }.bind(this)
     });
   },
-
   render: function(){
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" name="title" placeholder="link" onChange={this.fetchTitle} value={this.title}/>
-        <input type="text" name="link" value={this.link}/> <br />
-        <textarea name="description" placeholder="description"></textarea>
-        <br />
-        <input type="submit" value="Save" />
+        <input type="text" name="title" placeholder="link" 
+          onBlur={this.fetchTitle} onChange={this.updateTitle} value={this.state.title}/>
+        <input type="text" name="link" value={this.state.link} hidden/> <br />
+        <textarea name="description" placeholder="description" onChange={this.updateDescription} value={this.state.description}/>
+        <button>Save</button>
       </form>
     );
   }
@@ -61,16 +66,10 @@ var LinkList = React.createClass({
   getInitialState: function() {
     return {links: []};
   },
-
-  handleSubmit: function(title, link, description) {
-    var nextLinks = this.state.links.concat([{
-      title: title,
-      link: link,
-      description: description
-    }]);
+  handleSubmit: function(newLink) {
+    var nextLinks = this.state.links.concat([newLink]);
     this.setState({links: nextLinks});
   },
-
   render: function() {
     return (
       <section>
