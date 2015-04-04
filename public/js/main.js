6c82b40fb0ba57f38ceed2f8626156238e3a5b77
Tristan <thrawst88@gmail.com>
2015-04-04 17:28:41 -0600

    single-page-appiness with react-router

--- public/js/main.js
+++ public/js/main.js
@@ -69,6 +69,9 @@ var AddLinkForm = React.createClass({
 });
 
 var LinkList = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.func
+  },
   getInitialState: function() {
     return {links: []};
   },
@@ -77,15 +80,124 @@ var LinkList = React.createClass({
     this.setState({links: nextLinks});
   },
   render: function() {
+    //var listTitle = $.ajax ... get title with id
+    //this.setState({listTitle:listTitle});
     return (
-      <section id="list">
+      <section id="linkList">
+        <RouteHandler/>
+        { /*<h1>{this.state.linkTitle}</h1>*/ }
         <LinksBox links={this.state.links} />
         <AddLinkForm onLinkSubmit={this.handleSubmit} />
       </section>
     );
   }
 });
-React.render(
-  <LinkList />,
-  document.getElementById('content')
+
+//collection
+var ColsBox = React.createClass({
+  render: function() {
+    var createItem = function(item) {
+      return (
+        <li>
+          <Link to="list" params={{id: item.id}}>{item.title}</Link>
+        </li>
+      );
+    };
+    return (<ul>{this.props.links.map(createItem)}</ul>);
+  }
+});
+
+var AddCollectionForm = React.createClass({
+  getInitialState: function(){
+    return {id: this.genId(), title:''};
+  },
+  genId: function(){
+    return Math.floor(Math.random()*0xaabbcc);
+  },
+  handleChange: function(e){
+    this.setState({title: e.target.value});
+  },
+  handleSubmit: function(e){
+    if (!this.state.title){
+      return;
+    }
+    this.props.onLinkSubmit(this.state);
+    this.props.toggler();
+  },
+  render: function(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value={this.state.title} autofocus/>
+        <button>Add</button>
+        <button onClick={this.props.toggler}>Cancel</button>
+      </form>
+    );
+  }
+});
+
+var CollectionList = React.createClass({
+  getInitialState: function() {
+    return {cols: [], colFormVis: false};
+  },
+  handleSubmit: function(newCol) {
+    var newCols = this.state.cols.concat([newCol]);
+    this.setState({cols: newCols});
+  },
+  toggleForm: function(){
+    this.setState({colFormVis: !this.state.colFormVis});
+  },
+  render: function() {
+    return (
+      <section id="collectionList">
+        <RouteHandler/>
+        <ColsBox links={this.state.cols} />
+        { this.state.colFormVis ? <AddCollectionForm onLinkSubmit={this.handleSubmit} toggler={this.toggleForm} /> : null }
+        { !this.state.colFormVis ? <button onClick={this.toggleForm}>+</button> : null }
+      </section>
+    );
+  }
+});
+
+//router
+var Router = ReactRouter;
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <header>
+          <ul>
+            <li><Link to="collections">Collections</Link></li>
+          </ul>
+        </header>
+
+        <RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="collections" handler={CollectionList}/>
+    <Route name="list" path="/list/:id" handler={LinkList}/>
+  </Route>
 );
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('content'));
+});
+
+// Router.run(routes, Router.HistoryLocation, function (Handler) {
+//   React.render(<Handler/>, document.getElementById('content'));
+// });
+
+// React.render(
+//   <LinkList />,
+//   document.getElementById('content')
+// );
