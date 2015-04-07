c84929f67a6ca5e0d9f5acb4eb847b9ec0027515
Tristan <thrawst88@gmail.com>
2015-04-07 01:05:11 -0600

    CRUD ops - modifies a single object on the server, move this to some orm now

--- application/app.js
+++ application/app.js
@@ -7,11 +7,13 @@ app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var collections = {};
+
 app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
-app.post('/title', function(req, res){
+app.post('/api/title', function(req, res){
   var url = req.body.url;
   request(url, function (err, resquest, body) {
     if (!err && res.statusCode == 200) {
@@ -25,5 +27,40 @@ app.post('/title', function(req, res){
     }
   });
 });
+
+app.route('/api/collections')
+  .get(function(req, res){
+    var objs = [];
+    Object.keys(collections).forEach(function(el){
+      objs.push({id: el, title: collections[el].title});
+    });
+    res.send(objs);
+  });
+
+app.route('/api/collection')
+  .get(function(req, res){
+    res.send(collections[req.query.id]);
+  })
+  .post(function(req, res){
+    collections[req.body.newId] = {title: req.body.title, links: []};
+  })
+  .delete(function(req, res){
+    delete collections[req.body.id];
+  });
+
+app.route('/api/link')
+  .get(function(req, res){
+    console.log("nothing here");
+  })
+  .post(function(req, res){
+    collections[req.body.id].links.push(req.body.item);
+  })
+  .delete(function(req, res){
+    var index = req.body.index;
+    var id = req.body.colId;
+    var col = collections[id];
+    col.links.splice(index, 1);
+    collections[id] = col;
+  });
 
 module.exports = app;
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,20 @@
 var LinksBox = React.createClass({
   render: function() {
-    var createItem = function(item) {
+    var createItem = function(item, index) {
       return [
         (<dt><a href={item.link} target="_blank">{item.title}</a></dt>),
-        (<dd>{item.description}</dd>)
+        (
+          <dd>
+            <p>{item.description}</p>
+            <button onClick={this.deleteItem} value={index}>x</button>
+          </dd>
+        )
       ];
     };
-    return <dl>{this.props.links.map(createItem)}</dl>;
+    return <dl>{this.props.links.map(createItem, this)}</dl>;
+  },
+  deleteItem: function(e){
+    this.props.deleteItem(e.target.value);
   }
 });
 
@@ -40,7 +48,7 @@ var AddLinkForm = React.createClass({
     }
     this.setState({title: link});
     $.ajax({
-      url: '/title',
+      url: '/api/title',
       dataType: 'json',
       type: 'POST',
       data: {url: link}, //problem here?
@@ -73,20 +81,58 @@ var LinkList = React.createClass({
     router: React.PropTypes.func
   },
   getInitialState: function() {
-    return {links: []};
+    return {links: [], title: ''};
+  },
+  componentDidMount: function(){
+    var id = this.context.router.getCurrentParams().id;
+    $.ajax({
+      url: '/api/collection',
+      dataType: 'json',
+      type: 'GET',
+      data: {id: id},
+      success: function(data) {
+        this.setState({id: id, title: data.title, links: data.links});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
   },
   handleSubmit: function(newLink) {
+    var id = this.state.id;
+    $.ajax({
+      url: '/api/link',
+      dataType: 'json',
+      type: 'POST',
+      data: {id: id, item: newLink},
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
     var nextLinks = this.state.links.concat([newLink]);
     this.setState({links: nextLinks});
   },
+  handleDelete: function(index){
+    var id = this.state.id;
+    $.ajax({
+      url: '/api/link',
+      dataType: 'json',
+      type: 'DELETE',
+      data: {colId: id, index: index},
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
+    var newLinks = this.state.links;
+    newLinks.splice(index, 1);
+    this.setState({links: newLinks});
+  },
   render: function() {
-    //var listTitle = $.ajax ... get title with id
-    //this.setState({listTitle:listTitle});
     return (
       <section id="linkList">
         <RouteHandler/>
-        { /*<h1>{this.state.linkTitle}</h1>*/ }
-        <LinksBox links={this.state.links} />
+        <h1>{this.state.title}</h1>
+        <LinksBox links={this.state.links} deleteItem={this.handleDelete} />
         <AddLinkForm onLinkSubmit={this.handleSubmit} />
       </section>
     );
@@ -96,20 +142,27 @@ var LinkList = React.createClass({
 //collection
 var ColsBox = React.createClass({
   render: function() {
-    var createItem = function(item) {
+    var createItem = function(item, index) {
       return (
         <li>
           <Link to="list" params={{id: item.id}}>{item.title}</Link>
+          <button onClick={this.deleteItem} value={index}>x</button>
         </li>
       );
     };
-    return (<ul>{this.props.links.map(createItem)}</ul>);
+    return (<ul>{this.props.links.map(createItem, this)}</ul>);
+  },
+  deleteItem: function(e){
+    this.props.deleteItem(e.target.value);
   }
 });
 
 var AddCollectionForm = React.createClass({
   getInitialState: function(){
     return {id: this.genId(), title:''};
+  },
+  componentDidMount: function(){
+    this.refs.titleInput.getDOMNode().focus(); 
   },
   genId: function(){
     return Math.floor(Math.random()*0xaabbcc);
@@ -118,7 +171,7 @@ var AddCollectionForm = React.createClass({
     this.setState({title: e.target.value});
   },
   handleSubmit: function(e){
-    if (!this.state.title){
+    if (!/\S/.test(this.state.title)){
       return;
     }
     this.props.onLinkSubmit(this.state);
@@ -127,7 +180,7 @@ var AddCollectionForm = React.createClass({
   render: function(){
     return (
       <form onSubmit={this.handleSubmit}>
-        <input onChange={this.handleChange} value={this.state.title} autofocus/>
+        <input onChange={this.handleChange} value={this.state.title} ref="titleInput"/>
         <button>Add</button>
         <button onClick={this.props.toggler}>Cancel</button>
       </form>
@@ -139,8 +192,45 @@ var CollectionList = React.createClass({
   getInitialState: function() {
     return {cols: [], colFormVis: false};
   },
+  componentDidMount: function(){
+    $.ajax({
+      url: '/api/collections',
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        this.setState({cols:data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
+  },
   handleSubmit: function(newCol) {
+    $.ajax({
+      url: '/api/collection',
+      dataType: 'json',
+      type: 'POST',
+      data: {newId: newCol.id, title: newCol.title},
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
     var newCols = this.state.cols.concat([newCol]);
+    this.setState({cols: newCols});
+  },
+  handleDelete: function(index){
+    var id = this.state.cols[index].id;
+    $.ajax({
+      url: '/api/collection',
+      dataType: 'json',
+      type: 'DELETE',
+      data: {id: id},
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+    });
+    var newCols = this.state.cols;
+    newCols.splice(index, 1);
     this.setState({cols: newCols});
   },
   toggleForm: function(){
@@ -150,7 +240,7 @@ var CollectionList = React.createClass({
     return (
       <section id="collectionList">
         <RouteHandler/>
-        <ColsBox links={this.state.cols} />
+        <ColsBox links={this.state.cols} deleteItem={this.handleDelete} />
         { this.state.colFormVis ? <AddCollectionForm onLinkSubmit={this.handleSubmit} toggler={this.toggleForm} /> : null }
         { !this.state.colFormVis ? <button onClick={this.toggleForm}>+</button> : null }
       </section>
