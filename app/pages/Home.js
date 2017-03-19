955847f7bec01a28e0ede3e287c5cbe5a4311cbd
Tristan <thrawst88@gmail.com>
2017-03-19 15:35:43 -0600

    updated home page

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -5,12 +5,12 @@ const home = (props) => {
   return (
     <div className="homeStyle">
       <p>Suplmntl is an easy way to annotate and share links.</p>
-      <ul>
+      <ul className="hero">
         <li>
           Had a great conversation where you mentioned tons of things you&apos;ve
-          been reading, compile and share them with Suplmntl.
+          been reading? Compile and share them with Suplmntl.
         </li>
-        <li>Compiling sources for a paper you&apos;re writing, collect them with Suplmntl.</li>
+        <li>Gathering sources for a paper you&apos;re writing? Collect and organize them with Suplmntl.</li>
       </ul>
       <Link className="headerLink" to="sign-up">Sign Up</Link>
     </div>
