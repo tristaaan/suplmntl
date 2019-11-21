dfe72dc879758c270315f2edbf79a59f7d138dfc
Tristan <thrawst88@gmail.com>
2019-11-21 14:15:55 -0700

    upgraded to latest packages, builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,6 @@
-var webpack = require('webpack');
-var path = require('path');
-
-function nodeEnv() {
-  if (process.env.NODE_ENV) {
-    return '\'' + process.env.NODE_ENV + '\'';
-  }
-  return '\'development\'';
-}
-
-var definePlugin = new webpack.DefinePlugin({
-  'process.env.NODE_ENV': nodeEnv(),
-});
+const path = require('path');
 
 module.exports = {
-  plugins: [definePlugin],
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist/js'),
@@ -22,15 +9,9 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+        use: ['babel-loader']
       }
     ]
   }
