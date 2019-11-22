f865342dd2ddaf7db471f3d72d17d93ad12f6682
Tristan <thrawst88@gmail.com>
2019-11-21 17:30:51 -0700

    move conditional to callback

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { useCookies } from 'react-cookie';
 import store from './redux';
 import App from './app';
+import { loggedIn } from './redux/actions/auth';
+
+const [cookies] = useCookies(['token']);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('content')
+  document.getElementById('content'),
+  () => {
+    if (cookies.token) {
+      store.dispatch(loggedIn(cookies.token));
+    }
+  }
 );
