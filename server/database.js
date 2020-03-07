819336088a342fecbe1a35ddd0fea2b12524a863
Tristan <thrawst88@gmail.com>
2020-03-06 23:23:17 -0700

    fix deprecation warnings

--- server/database.js
+++ server/database.js
@@ -22,9 +22,17 @@ if (process.env.MONGODB_URI) {
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoURI, (err) => {
-  if (err) { throw err; }
-  console.log('db connected...');
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+const db = mongoose.connection;
+db.on('error',
+  console.error.bind(console, 'connection error:')
+);
+db.once('open', function() {
+  console.log('connected to database...');
 });
 
 function strId() {
